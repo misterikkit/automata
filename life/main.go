abfe2d9964670839de7ad845aa48bd126f13aea5
Jonathan Basseri <misterikkit@gmail.com>
2021-02-15 16:27:10 -0800

    comment main

--- life/main.go
+++ life/main.go
@@ -15,11 +15,16 @@ import (
 
 func main() {
 	defer fmt.Println("bye!")
+	// Set random seed
 	seed := time.Now().Unix()
 	defer fmt.Printf("Seed: %v\n", seed)
 	rand.Seed(seed)
+
+	// Create game context
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// Initialize gene
 	var gn gene.Gene
 	if len(os.Args) > 1 {
 		gn = gene.FromString(os.Args[1])
@@ -29,9 +34,11 @@ func main() {
 	defer fmt.Printf("Gene: %+v\n", gn)
 	rule := gn.AsRule()
 
+	// Initialize Game
 	g := game.New(20, 30)
-	g = g.Next(game.Random)
+	// g = g.Next(game.Random)
 
+	// Initialize ui, and wire events
 	pauseCh := make(chan struct{}, 1)
 	t, err := tui.New(fmt.Sprintf("Gene %v\tEsc to exit", gn), func(e tui.Event) {
 		switch e {
@@ -45,11 +52,11 @@ func main() {
 		log.Fatal(err)
 	}
 	defer t.Close()
-	t.DrawGame(g)
+	t.DrawGame(g) // draw initial state
 
+	// Run the game loop
 	tick := time.NewTicker(100 * time.Millisecond)
 	defer tick.Stop()
-
 	paused := false
 loop:
 	for {
@@ -60,11 +67,12 @@ loop:
 			}
 			g = g.Next(rule)
 			t.DrawGame(g)
+
 		case <-pauseCh:
 			paused = !paused
+
 		case <-ctx.Done():
 			break loop
 		}
 	}
-
 }
