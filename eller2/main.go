6be91273767d55f58ba99863d874644aa91a84bf
Jonathan Basseri <misterikkit@gmail.com>
2021-02-25 23:27:36 -0800

    Add comments and make probability easier to tune
    Defined func p(p float64) bool which returns true with probability p,
    such that the east-west wall removal probability can be tweaked with a
    single number.

--- eller2/main.go
+++ eller2/main.go
@@ -9,15 +9,19 @@ import (
 	"github.com/misterikkit/automata/wall"
 )
 
+// state represents one row of the maze, which is all that the Eller algorithm
+// needs in memory at any time.
 type state struct {
-	// which set does cell i belong to
+	// list of group IDs indexed by cell position
 	groupIDs []int
-	// which indexes are in group g
-	groups    map[int][]int
+	// list of cell positions indexed by group ID
+	groups map[int][]int
+	// Whether the east/south wall is open for the cell at that position
 	openEast  []bool
 	openSouth []bool
 }
 
+// new instantiates a fresh state.
 func new(cols int) *state {
 	s := &state{
 		groupIDs:  make([]int, cols),
@@ -29,12 +33,16 @@ func new(cols int) *state {
 	return s
 }
 
+// compute randomly removes walls between cells, causing groups to merge, then
+// randomly selects 1 or more cell from each group to advance to the next row
+// (by removing its south wall).
 func (s *state) compute(lastRow bool) {
 	for i := 0; i < len(s.groupIDs)-1; i++ {
 		if s.groupIDs[i] == s.groupIDs[i+1] {
 			continue
 		}
-		if lastRow || rand.Intn(2) == 0 {
+		// On the last row, we connect all isloated subsections of the maze.
+		if lastRow || p(0.5) {
 			// log.Printf("Merge %v and %v", i, i+1)
 			s.openEast[i] = true
 			s.replace(s.groupIDs[i+1], s.groupIDs[i])
@@ -53,6 +61,8 @@ func (s *state) compute(lastRow bool) {
 	}
 }
 
+// replace merges two groups, replacing all references to the old group with the
+// new group.
 func (s *state) replace(old, new int) {
 	s.groups[new] = append(s.groups[new], s.groups[old]...)
 	delete(s.groups, old)
@@ -63,6 +73,9 @@ func (s *state) replace(old, new int) {
 	}
 }
 
+// nextRow resets the state in preparation for computing the next row, by
+// - creating new group IDs for cells that don't advance to the next row
+// - resetting all walls
 func (s *state) nextRow() {
 	for i := range s.groupIDs {
 		if !s.openSouth[i] {
@@ -82,6 +95,9 @@ func (s *state) nextRow() {
 	}
 }
 
+// removeOne removes a single cell position from a group, and sets that cell's
+// group to 0 (invalid). This is common when some members of a group do not
+// advance to the next row. If a group becomes empty, it is completely deleted.
 func (s *state) removeOne(pos, groupID int) {
 	newGroup := []int{}
 	for _, p := range s.groups[groupID] {
@@ -98,19 +114,6 @@ func (s *state) removeOne(pos, groupID int) {
 	s.groupIDs[pos] = 0
 }
 
-func max(vs []int) int {
-	if len(vs) == 0 {
-		return 0
-	}
-	val := vs[0]
-	for _, v := range vs {
-		if v > val {
-			val = v
-		}
-	}
-	return val
-}
-
 func main() {
 	h := flag.Int("h", 5, "height")
 	w := flag.Int("w", 5, "width")
@@ -119,12 +122,14 @@ func main() {
 
 	s := new(*w)
 	// log.Println(s)
+	// wall.Maze is a utility for pretty-printing mazes.
 	maze := wall.NewMaze(*h, *w)
 
 	for r := 0; r < *h; r++ {
 		lastRow := r+1 == *h
 		s.compute(lastRow)
 		// log.Println(s)
+		// Copy computed row into the wall.Maze
 		for c := 0; c < *w; c++ {
 			// maze.Set(r, c, asID(s.groupIDs[c]))
 			if s.openEast[c] {
@@ -140,7 +145,26 @@ func main() {
 	fmt.Println(maze)
 }
 
+func max(vs []int) int {
+	if len(vs) == 0 {
+		return 0
+	}
+	val := vs[0]
+	for _, v := range vs {
+		if v > val {
+			val = v
+		}
+	}
+	return val
+}
+
+// convenience function to create single-character IDs for debugging the mazes
 func asID(i int) string {
 	const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	return string(chars[i%len(chars)])
+}
+
+// p returns true with probability equal to p
+func p(p float64) bool {
+	return rand.Float64() < p
 }
